1054ae2bc6409318943d6803f2ec6960c76aac32
farhannurr <farhannurramadhan3@gmail.com>
2021-09-26 13:11:48 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo " Nama Saya Farhan Nur Ramadhan tinggal di cikarang
+        olahraga yang saya sukai adalah jogging";
+    }
+}
